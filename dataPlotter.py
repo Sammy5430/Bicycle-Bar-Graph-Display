f2ee616b0a2a81e340d78cf6a54344c735e7ac6c
Unknown <samuel.gonzalez9@upr.edu>
2019-10-23 10:20:35 -0400

    Improved displayed data
    Separated each graph into its own plot, added colors to each plot, added a title to each plot, changed method to use a dictionary of arrays containing measurements of each value to be displayed on the graph

--- dataPlotter.py
+++ dataPlotter.py
@@ -1,26 +1,100 @@
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from matplotlib import style
+import random
+from matplotlib import animation
 
-style.use('fivethirtyeight')
+x_axis = ['Voltage', 'Current', 'Power', 'RPM', 'Energy']
+nf = 30  #Number of frames
+fig = plt.figure(figsize=(15, 7))
 
-fig = plt.figure()
-ax1 = fig.add_subplot(1, 1, 1)
+plt.subplot(151)
+xv = plt.bar('Voltage', 20, color='red')
+plt.autoscale(enable=True, axis='y')
+plt.title('Voltage (V)')
+
+plt.subplot(152)
+xc = plt.bar('Current', 30, color='orange')
+plt.autoscale(enable=True, axis='y')
+plt.title('Current (A)')
+
+plt.subplot(153)
+xp = plt.bar('Power', 40, color='yellow')
+plt.autoscale(enable=True, axis='y')
+plt.title('Power (W)')
+
+plt.subplot(154)
+xe = plt.bar('RPM', 30, color='green')
+plt.autoscale(enable=True, axis='y')
+plt.title('RPM')
+
+plt.subplot(155)
+xr = plt.bar('Energy', 20, color='cyan')
+plt.autoscale(enable=True, axis='y')
+plt.title('Energy (W/h)')
+
+count = 0
 
 
-def animate(i):
-    graph_data = open('testData.txt', 'r').read()
+#clear file
+#############################################
+f = open('testData.txt', 'w')
+f.write('')
+f.close()
+#############################################
+
+
+#populate file with random data
+###################################################################################
+counter = 0
+f = open('testData.txt', 'a')
+while counter < 1000:
+    strtext = "" + x_axis[counter % 5] + ", " + str(random.randint(0, 25)) + "\n"
+    f.write(strtext)
+    counter += 1
+f.close()
+###################################################################################
+
+
+def barlist():
+    graph_data = open('testData.txt', 'r').read()       #dont need to read the whole file in every iteration
     lines = graph_data.split('\n')
-    xs = []
-    ys = []
+    v_arr = []
+    c_arr = []
+    p_arr = []
+    r_arr = []
+    e_arr = []
     for line in lines:
         if len(line) > 1:
             x, y = line.split(',')
-            xs.append(float(x))
-            ys.append(float(y))
-    ax1.clear()
-    ax1.plot(xs, ys)
+            if x == 'Voltage':
+                v_arr.append(float(y))
+            elif x == 'Current':
+                c_arr.append(float(y))
+            elif x == 'Power':
+                p_arr.append(float(y))
+            elif x == 'RPM':
+                r_arr.append(float(y))
+            elif x == 'Energy':
+                e_arr.append(float(y))
+    values = {'Voltage': v_arr,
+              'Current': c_arr,
+              'Power': p_arr,
+              'RPM': r_arr,
+              'Energy': e_arr}
+    return values
 
 
-ani = animation.FuncAnimation(fig, animate, interval=1000)
+def animate(i):
+    global count, xv, xc, xp, xe, xr
+    height_vals = barlist()
+    xv[0].set_height(height_vals['Voltage'][count])
+    xc[0].set_height(height_vals['Current'][count])
+    xp[0].set_height(height_vals['Power'][count])
+    xr[0].set_height(height_vals['RPM'][count])
+    xe[0].set_height(height_vals['Energy'][count])
+    count = (count + 1) % (len(height_vals['Energy'])-1)
+
+
+anim = animation.FuncAnimation(fig, animate, repeat=False, blit=False, interval=nf)
+
 plt.show()
+
